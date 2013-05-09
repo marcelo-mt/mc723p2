14ec4a17761247a13da050cb9466141cb45bfd9b
mingatos <marcelo.m.toledo@gmail.com>
2013-05-09 02:56:39 -0300

    branchpredictor.h

--- branchpredictor.h
+++ branchpredictor.h
@@ -1,10 +1,26 @@
 #include <iostream>
 
+#define STRONG_N_TAKEN		0
+#define WEAK_N_TAKEN		1
+#define WEAK_TAKEN 			2
+#define STRONG_TAKEN 		3
+
 using namespace std;
 
-typedef struct _
+typedef struct _branch_prediction_buffer
+{
+	long int address;
+	int saturating_counter = WEAK_TAKEN;
+
+} BPB;
 
 class TwoBitPredictor {
+	
+	vector < BPB > branch_prediction_buffer;
+	
+	dint last_prediction_index = -1;
+
+
 	public:
 		
 
