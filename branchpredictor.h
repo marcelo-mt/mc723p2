36d1628b14e1ef1f5c64beffbebcfe157fee706a
mingatos <marcelo.m.toledo@gmail.com>
2013-05-09 00:42:20 -0300

    Pega essa

---
+++ branchpredictor.h
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct _
+
+class TwoBitPredictor {
+	public:
+		
+
+		fase ();
+		fase (int);
+		bool init;
+		bool regWrite;
+		bool memRead;
+		int inst;
+		int rs, rt, rd;
+		int imm;
+		int addr;
+
+};
